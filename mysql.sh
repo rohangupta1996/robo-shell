464a4ab1f50fbf990e2b4b47f57611c912cf2c34
Rohan <rgrohangupta65@gmail.com>
2023-08-29 17:52:19 +0530

    Roboshop component

--- mysql.sh
+++ mysql.sh
@@ -1,11 +1,10 @@
-source Common.sh
+source common.sh
 
 mysql_root_password=$1
 if [ -z "${mysql_root_password}" ]; then
-  echo "Missing mysql root password"
+  echo -e "\e[31mMissing MySQL Root Password argument\e[0m"
   exit 1
 fi
-
 
 print_head "Disabling MySQL 8 Version"
 dnf module disable mysql -y  &>>${log_file}
@@ -27,11 +26,9 @@ print_head "Start MySQL Service"
 systemctl start mysqld  &>>${log_file}
 status_check $?
 
-print_head "Start MySQL Service"
+print_head "Set Root Password"
 echo show databases | mysql -uroot -p${mysql_root_password} &>>${log_file}
 if [ $? -ne 0 ]; then
-  mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log_file}
+  mysql_secure_installation --set-root-pass ${mysql_root_password}  &>>${log_file}
 fi
 status_check $?
-
-
