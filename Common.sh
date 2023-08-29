1b7414b054e2345b2eac1b01adf388a26f0251ca
Rohan <rgrohangupta65@gmail.com>
2023-08-29 17:37:05 +0530

    Roboshop component

--- Common.sh
+++ Common.sh
@@ -56,7 +56,7 @@ schema_setup() {
     status_check $?
 
     print_head "Load Schema"
-    mongo --host mongodb-dev.rohandevops.online </app/schema/catalogue.js &>>${log_file}
+    mongo --host mongodb-dev.rohandevops.online </app/schema/${component}.js &>>${log_file}
     status_check $?
 
   elif [ "${schema_type}" == "mysql" ]; then
